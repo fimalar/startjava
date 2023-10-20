c965edbf19d2f72d2317bbe7a5441567278c8a42
fimalar <nebasineva12@gmail.com>
2023-10-20 13:34:57 +0300

    Добавила .gitignore с шаблоном *.class

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 
 public class MyFirstApp {
     public static void main (String[] arg) {
+        System.out.println("У какого языка программирования следующий слоган:");
         System.out.println("Write once, run anywhere");
         System.out.print("Написано однажды, ");
         System.out.println("работает везде");
-        System.out.println(Charset.defaultCharset().displayName());
 
+        String answer = new Scanner(System.in).next();
+        if (answer.equalsIgnoreCase("Java")) {
+            System.out.println("Вы угадали");
+        } else System.out.println("Увы, но - это Java");
+
+
+        System.out.println(Charset.defaultCharset().displayName());
         Scanner consol = new Scanner(System.in, "cp866");
         System.out.println("Введите свое имя: ");
         System.out.println(consol.nextLine());
