ad163d90b0ded632d49272b89f59e040c9584dc1
fimalar <148458550+fimalar@users.noreply.github.com>
2023-10-20 12:53:27 +0300

    Изменила вывод текста отображаемого в консоли, удалила пустые строки в конце

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 public class MyFirstApp {
     public static void main (String[] arg) {
         System.out.println("Write once, run anywhere");
-        System.out.println("Написано однажды, работает везде");
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде");
         System.out.println(Charset.defaultCharset().displayName());
 
         Scanner consol = new Scanner(System.in, "cp866");
         System.out.println("Введите свое имя: ");
         System.out.println(consol.nextLine());
-
-
     }  
 }
