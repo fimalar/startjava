1390ee3887e3f62897b66f0edfbf6b0489319165
fimalar <nebasineva12@gmail.com>
2023-10-22 06:49:40 +0300

    Изменение имен переменных, добавлены пустые строки между блоками
    percentageOfDeposit -> persentDeposit
    mathematicalExpression -> mathExpression
    chooseSomething -> choose
    numberOfPoints -> score
    eBookTitle-> ebookTitle
    capacityOfBus -> capacity
    isHundredsEqual -> sEqualHundreds
    isComputerTurnedOn -> isPcOn
    isAnythingCreated -> isCreated
    isEmailValid -> isValidEmail
    isUniqueStrings -> hasUniqueStrings
    uuid -> oldUuid
    url -> reducedUrl
    cuid -> newCuid
    
    Убран Of в наименования, чтобы имена содержали только значимые слова
    sumOfNumbers -> sumNumbers
    productDigits -> productDigits
    quantityOfTens -> quantityTens
    weightOfDog -> weightDog
    countOfZeros -> countZeros
    costOfCoffeeTakeAway -> costCoffeeTakeAway
    endOfRange -> endRange
    fullNameOfEmployeeOfMonth -> fullNameEmployeeMonth
    capacityOfBus -> capacity
    amountOfNumbersEachRow -> amountNumbersEachRow

--- src/VariableNamesTheme.java
+++ src/VariableNamesTheme.java
@@ -1,22 +1,22 @@
 public class VariableNamesTheme {
     public static void main(String[] args) {
-        System.out.println("1. Разные переменные");
+        System.out.println("\n1. Разные переменные");
         // цифра
         int digit = 4;
         // сумма чисел
-        int sumOfNumbers = 10 + 20;
+        int sumNumbers = 10 + 20;
         // произведение цифр
-        double productOfDigits = 1.42 * 4.1;
+        double productDigits = 1.42 * 4.1;
         // максимальное число
         int maxNumber = 100;
         // количество десятков
-        int quantityOfTens = 7;
+        int quantityTens = 7;
         // вес собаки
-        double weightOfDog = 32.4;
+        double weightDog = 32.4;
         // исходное число
         int initialNumber = 1;
         // процент по вкладу
-        double percentageOfDeposit = 6.8;
+        double persentDeposit = 6.8;
         // (переменная хранит символ) &
         char ampersand = '&';
         // код ошибки
@@ -24,80 +24,81 @@ public class VariableNamesTheme {
         // тип сообщения
         String messageType = "Предупреждение";
         // число нулей
-        short countOfZeros = 37;
+        short countZeros = 37;
         // уникальное число
         int uniqueNumber = 777;
         // случайное число
         long randomNumber = 123456789L;
         // математическое выражение
-        String mathematicalExpression  =  "(4x + y - 7) / 2";
+        String mathExpression = "(4x + y - 7) / 2";
         // выбор (чего-либо)
-        String chooseSomething = "Определитесь с выбором";
+        String choose = "Определитесь с выбором";
         // количество очков (в игре)
-        int numberOfPointsInGame = 10;
+        int score = 10;
         // максимальная длина строки
         int maxStringLength = 255;
         // пункт меню
         int menuItem = 3;
         // стоимость кофе на вынос
-        int costOfCoffeeTakeAway = 320;
+        int costCoffeeTakeAway = 320;
         // дата начала (чего-либо)
         String startDate = "01 января 2020";
         // окончание диапазона
-        int endOfRange = 10000;
+        int endRange = 10000;
         // полное имя работника месяца
-        String fullNameOfEmployeeOfMonth = "Иванов Иван Иванович";
+        String fullNameEmployeeMonth = "Иванов Иван Иванович";
         // заголовок электронной книги
-        String eBookTitle = "Пролог";
+        String ebookTitle = "Пролог";
         // размер
         int size = 16;
         // вместимость (чего-либо)
-        int capacityOfBus = 32;
+        int capacity = 32;
         // счетчик
         int counter = 0;
         // путь до файла
         String filePath = "C:\\User\\Documents\\plan.txt";
         // количество чисел в каждой строке
-        int amountOfNumbersEachRow = 0;
+        int amountNumbersEachRow = 0;
+
         System.out.println("Цифра = " + digit);
-        System.out.println("Сумма чисел = " + sumOfNumbers);
-        System.out.println("Произведение цифр = " + productOfDigits);
+        System.out.println("Сумма чисел = " + sumNumbers);
+        System.out.println("Произведение цифр = " + productDigits);
         System.out.println("Максимальное число = " + maxNumber);
-        System.out.println("Количество десятков = " + quantityOfTens);
-        System.out.println("Вес собаки - " + weightOfDog + " кг");
+        System.out.println("Количество десятков = " + quantityTens);
+        System.out.println("Вес собаки - " + weightDog + " кг");
         System.out.println("Исходное число = " + initialNumber);
-        System.out.println("Процент по вкладу составляет - " + percentageOfDeposit + " %");
+        System.out.println("Процент по вкладу составляет - " + persentDeposit + " %");
         System.out.println("Переменная хранит символ - '" + ampersand + "'");
         System.out.println("Код ошибки: " + errorCode);
         System.out.println("Тип сообщения: " + messageType);
-        System.out.println("Число нулей = " + countOfZeros);
+        System.out.println("Число нулей = " + countZeros);
         System.out.println("Уникальное число = " + uniqueNumber);
         System.out.println("Случайное число = " + randomNumber);
-        System.out.println("Математическое выражение: " + mathematicalExpression);
-        System.out.println("Внимание! " + chooseSomething);
-        System.out.println("Количество очков = " + numberOfPointsInGame);
+        System.out.println("Математическое выражение: " + mathExpression);
+        System.out.println("Внимание! " + choose);
+        System.out.println("Количество очков = " + score);
         System.out.println("Максимальная длина строки: " + maxStringLength);
         System.out.println("Пункт меню N " + menuItem);
-        System.out.println("Стоимость кофе на вынос = " + costOfCoffeeTakeAway);
+        System.out.println("Стоимость кофе на вынос = " + costCoffeeTakeAway);
         System.out.println("Дата начала форума - " + startDate);
-        System.out.println("Окончание диапазона = " + endOfRange);
-        System.out.println("Полное имя работника месяца: " + fullNameOfEmployeeOfMonth);
-        System.out.println("Заголовок электронной книги: " + eBookTitle);
+        System.out.println("Окончание диапазона = " + endRange);
+        System.out.println("Полное имя работника месяца: " + fullNameEmployeeMonth);
+        System.out.println("Заголовок электронной книги: " + ebookTitle);
         System.out.println("Размер = " + size);
-        System.out.println("Вместимость автобуса = " + capacityOfBus);
+        System.out.println("Вместимость автобуса = " + capacity);
         System.out.println("Счетчик = " + counter);
         System.out.println("Путь до файла: " + filePath);
-        System.out.println("Количество чисел в каждой строке = " + amountOfNumbersEachRow);
+        System.out.println("Количество чисел в каждой строке = " + amountNumbersEachRow);
 
         System.out.println("\n2. boolean-переменные");
         // сотни равны?
-        boolean isHundredsEqual = true;
+        boolean isEqualHundreds = true;
         // компьютер включен?
-        boolean isComputerTurnedOn = false;
+        boolean isPcOn = false;
         // есть равные цифры?
         boolean isEqualNumbers = false;
         // (что-либо) создано?
-        boolean isAnythingCreated = false;
+        boolean isCreated = false;
         // (что-либо) пусто?
         boolean isEmpty = true;
         // (что-либо) активный?
@@ -105,40 +106,42 @@ public class VariableNamesTheme {
         // новый?
         boolean isNew = false;
         // электронная почта действительная?
-        boolean isEmailValid = false;
+        boolean isValidEmail = false;
         //имеются уникальные строки?
-        boolean isUniqueStrings = false;
-        System.out.println("Сотни равны? " + isHundredsEqual);
-        System.out.println("Компьютер включен? " + isComputerTurnedOn);
+        boolean hasUniqueStrings = false;
+
+        System.out.println("Сотни равны? " + isEqualHundreds);
+        System.out.println("Компьютер включен? " + isPcOn);
         System.out.println("Есть равные цифры? " + isEqualNumbers);
-        System.out.println("(Что-либо) создано? " + isAnythingCreated);
+        System.out.println("(Что-либо) создано? " + isCreated);
         System.out.println("(Что-либо) пусто? " + isEmpty);
         System.out.println("(Что-либо) активный? " + isActive);
         System.out.println("Новый? " + isNew);
-        System.out.println("Электронная почта действительная? " + isEmailValid);
-        System.out.println("Имеются уникальные строки? " + isUniqueStrings);
+        System.out.println("Электронная почта действительная? " + isValidEmail);
+        System.out.println("Имеются уникальные строки? " + hasUniqueStrings);
 
         System.out.println("\n3. Аббревиатуры");
         // старый universally unique identifier
-        String uuid = "0x3422b448-2460-4fd2-9183-8000de6f8343";
+        String oldUuid = "0x3422b448-2460-4fd2-9183-8000de6f8343";
         // производитель оперативной памяти
-        String manufacturerOfRam = "Crucial";
+        String manufacturerRam = "Crucial";
         // емкость жесткого диска
         String capacityHhd = "1 ТБ";
         // протокол передачи гипертекста
         String http = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
         // сокращенный uniform resource locator
-        String url = "www.domain.com:";
+        String reducedUrl = "www.domain.com:";
         // новый идентификатор клиента
-        int cuid = 456789;
+        int newCuid = 456789;
         // кодировка american standard code for information interchange
         String ascii = "американский стандартный код для обмена информацией";
-        System.out.println("Старый universally unique identifier - " + uuid);
-        System.out.println("Производитель оперативной памяти - " + manufacturerOfRam);
+
+        System.out.println("Старый universally unique identifier - " + oldUuid);
+        System.out.println("Производитель оперативной памяти - " + manufacturerRam);
         System.out.println("Емкость жесткого диска " + capacityHhd);
-        System.out.println("Пример - " +  http);
-        System.out.println("URL: " + url);
-        System.out.println("Новый идентификатор клиента - " + cuid);
+        System.out.println("Пример - " + http);
+        System.out.println("URL: " + reducedUrl);
+        System.out.println("Новый идентификатор клиента - " + newCuid);
         System.out.println("ASCII - " + ascii);
     }
 }
