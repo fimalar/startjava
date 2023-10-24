71188520b12deb84daef2fb18ca143752b9cdd44
fimalar <nebasineva12@gmail.com>
2023-10-24 20:48:38 +0300

    Изменены наименования переменных, char переменные инициализированы через символы

--- src/VariablesTheme.java
+++ src/VariablesTheme.java
@@ -22,16 +22,14 @@ public class VariablesTheme {
         System.out.println("\n2. Расчет стоимости товара со скидкой");
         int pricePen = 100;
         int priceBook = 200;
-        String currency = " руб.";
-        int totalDiscount = 11;
-        char chPercent = '%';
+        int discountPercent = 11;
         int totalPrice = pricePen + priceBook;
-        int sumTotalDiscaunt = totalPrice * 11 / 100;
-        int totalPriceWithDiscount = totalPrice - sumTotalDiscaunt;
+        int discountSum = totalPrice * 11 / 100;
+        int discountPrice = totalPrice - discountSum;
 
-        System.out.println("Общая стоимость товаров без скидки - " + totalPrice + currency);
-        System.out.println("Сумма скидки - " + sumTotalDiscaunt + currency);
-        System.out.println("Общая стоимость товаров со скидкой - " + totalPriceWithDiscount + currency);
+        System.out.println("Общая стоимость товаров без скидки - " + totalPrice + " руб.");
+        System.out.println("Сумма скидки - " + discountSum + " руб.");
+        System.out.println("Общая стоимость товаров со скидкой - " + discountPrice + " руб.");
 
         System.out.println("\n3. Вывод слова JAVA");
         System.out.println("   J    a  v     v  a   ");
@@ -63,76 +61,76 @@ public class VariablesTheme {
         System.out.println("Максимальное число char после декремента: " + (int) --maxChar);
 
         System.out.println("\n5. Перестановка значений переменных");
-        int two = 2;
-        int five = 5;
+        int a = 2;
+        int b = 5;
         
         System.out.println("С помощью третьей переменной:");
-        System.out.println("  исходные значения переменных: two = " + two + " five = " + five);
-        int temp = two;
-        two = five;
-        five = temp;
-        System.out.println("  новые значения переменных: two = " + two + " five = " + five);
+        System.out.println("  исходные значения переменных: a = " + a + " b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("  новые значения переменных: a = " + a + " b = " + b);
 
         System.out.println("С помощью арифметических операций:");
-        System.out.println("  исходные значения переменных: two = " + two + " five = " + five);
-        two = two + five;
-        five = two - five;
-        two = two - five;
-        System.out.println("  новые значения переменных: two = " + two + " five = " + five);
+        System.out.println("  исходные значения переменных: a = " + a + " b = " + b);
+        a += b;
+        b = a - b;
+        a -= b;
+        System.out.println("  новые значения переменных: a = " + a + " b = " + b);
 
         System.out.println("С помощью побитовой операции");
-        System.out.println("  исходные значения переменных: two = " + two + " five = " + five);
-        temp = two ^ five;
-        two = temp ^ two;
-        five = temp ^ five;
-        System.out.println("  новые значения переменных: two = " + two + " five = " + five);
+        System.out.println("  исходные значения переменных: a = " + a + " b = " + b);
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        System.out.println("  новые значения переменных: a = " + a + " b = " + b);
 
         System.out.println("\n6. Вывод символов и их кодов");
-        char ch36 = 36;
-        char ch42 = 42;
-        char ch64 = 64;
-        char ch124 = 124;
-        char ch126 = 126;
+        char dollar = '$';
+        char asterisk = '*';
+        char atSign = '@';
+        char verticalBar = '|';
+        char tilde = '~';
 
-        System.out.println("  " + (int) ch36 + "\t" + ch36);
-        System.out.println("  " + (int) ch42 + "\t" + ch42);
-        System.out.println("  " + (int) ch64 + "\t" + ch64);
-        System.out.println("  " + (int) ch124 + "\t" + ch124);
-        System.out.println("  " + (int) ch126 + "\t" + ch126);
+        System.out.println("  " + (int) dollar + "\t" + dollar);
+        System.out.println("  " + (int) asterisk + "\t" + asterisk);
+        System.out.println("  " + (int) atSign + "\t" + atSign);
+        System.out.println("  " + (int) verticalBar + "\t" + verticalBar);
+        System.out.println("  " + (int) tilde + "\t" + tilde);
 
         System.out.println("\n7. Вывод в консоль ASCII-арт Дюка");
-        char ch1 = '/';
-        char ch2 = '\\';
-        char ch3 = '_';
-        char ch4 = '(';
-        char ch5 = ')';
+        char slash = '/';
+        char backslash = '\\';
+        char underscore = '_';
+        char leftParenthesis = '(';
+        char rightParenthesis = ')';
 
-        System.out.println("    " + ch1 + ch2 + "    ");
-        System.out.println("   " + ch1 + "  " + ch2 + "   ");
-        System.out.println("  " + ch1 + ch3 + ch4 + " " + ch5 + ch2 + "  ");
-        System.out.println(" " + ch1 + "      " + ch2 + " ");
-        System.out.printf("%c%c%c%c%c%c%c%c%c%c\n", ch1, ch3, ch3, ch3, ch3, ch1, ch2, ch3, ch3, ch2);
+        System.out.println("    " + slash + backslash);
+        System.out.println("   " + slash + "  " + backslash);
+        System.out.println("  " + slash + underscore + leftParenthesis + " " + rightParenthesis + backslash);
+        System.out.println(" " + slash + "      " + backslash);
+        System.out.println(slash + underscore + underscore + underscore + underscore + slash + backslash + 
+                underscore + underscore + backslash);
 
         System.out.println("\n8. Вывод количества сотен, десятков и единиц числа");
         int number = 123;
-        int amountUnits = number % 10;
-        int amountTens = number / 10 % 10;
-        int amountHundreds = number / 100;
-        int multiplication = amountUnits * amountTens * amountHundreds;
-        int sum = amountUnits + amountTens + amountHundreds;
+        int ones = number % 10;
+        int tens = number / 10 % 10;
+        int hundreds = number / 100;
+        int multiplication = ones * tens * hundreds;
+        int sum = ones + tens + hundreds;
 
         System.out.println("Число " + number + " содержит: ");
-        System.out.println("  сотен - " + amountHundreds + "\n  десятков - " + amountTens + 
-                "\n  единиц - " + amountUnits);
+        System.out.println("  сотен - " + hundreds + "\n  десятков - " + tens + "\n  единиц - " + ones);
         System.out.println("Сумма его цифр равна = " + sum);
         System.out.println("Произведение = " + multiplication);
 
         System.out.println("\n9. Вывод времени");
-        int allSeconds = 86399;
-        int numberSeconds = allSeconds % 60;
-        int numberMinute = allSeconds / 60 % 60;
-        int numberHour = allSeconds / 60 / 60;
+        int allSs = 86399;
+        int ss = allSs % 60;
+        int mm = allSs / 60 % 60;
+        int hh = allSs / 60 / 60;
 
-        System.out.printf("%2d:%2d:%2d", numberHour, numberMinute, numberSeconds);
+        System.out.printf("%2d:%2d:%2d", hh, mm, ss);
     }
 }
