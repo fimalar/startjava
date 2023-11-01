928fd2d40493b6602e723360f8e30ec920870c73
fimalar <nebasineva12@gmail.com>
2023-11-01 15:17:53 +0300

    Переименованы переменные

--- src/CyclesTheme.java
+++ src/CyclesTheme.java
@@ -1,11 +1,11 @@
 public class CyclesTheme {
     public static void main(String[] args) {
         System.out.println("\n1. Подсчет суммы четных и нечетных чисел");
-        int borderLow = -10;
-        int borderUp = 21;
+        int startRange = -10;
+        int endRange = 21;
         int sumEven = 0;
         int sumOdd = 0;
-        int counter = borderLow;
+        int counter = startRange;
         do {
             if (counter % 2 == 0) {
                 sumEven += counter;
@@ -13,9 +13,9 @@ public class CyclesTheme {
                 sumOdd += counter;
             }
             counter++;
-        } while (counter <= borderUp);
-        System.out.printf("В отрезке [%1$d, %2$d] сумма четных чисел = %3$d, а нечетных = %4$d%n", borderLow, 
-            borderUp, sumEven, sumOdd);
+        } while (counter <= endRange);
+        System.out.printf("В отрезке [%1$d, %2$d] сумма четных чисел = %3$d, а нечетных = %4$d%n", startRange, 
+                endRange, sumEven, sumOdd);
 
         System.out.println("\n2. Вывод чисел в порядке убывания");
         int a = 10;
@@ -41,22 +41,22 @@ public class CyclesTheme {
         }
         
         System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр");
-        a = 1234;
-        int sum = 0;
-        while (a > 0) {
-            int numberOnes = a % 10;
-            sum += numberOnes;
-            System.out.print(numberOnes);
-            a /= 10;
+        int numberToReverse = 1234;
+        int sumDigits = 0;
+        while (numberToReverse > 0) {
+            int remainder = numberToReverse % 10;
+            sumDigits += remainder;
+            System.out.print(remainder);
+            numberToReverse /= 10;
         }
-        System.out.println("\nСумма цифр = " + sum);
+        System.out.println("\nСумма цифр = " + sumDigits);
     
         System.out.println("\n4. Вывод чисел в несколько строк");
-        borderLow = 1;
-        borderUp = 24;
+        startRange = 1;
+        endRange = 24;
         counter = 0;
 
-        for (int i = borderLow; i < borderUp; i++) {
+        for (int i = startRange; i < endRange; i++) {
             if (i % 2 != 0) {
                 System.out.printf("%4d ", i);
                 counter++;
@@ -64,7 +64,7 @@ public class CyclesTheme {
                     System.out.println();
                 }
             }
-            if (i == (borderUp - 1) && counter % 5 != 0) {
+            if (i == (endRange - 1) && counter % 5 != 0) {
                 for (int j = 5 - (i % 5); j >= 0; j--) {
                     System.out.printf("%1$4c ", '0');
                 }
@@ -72,16 +72,16 @@ public class CyclesTheme {
         }
 
         System.out.println("\n\n5. Проверка количества двоек числа на четность/нечетность");
-        int number = 3242592;
+        int numberCheckTwos = 3242592;
         int countTwos = 0;
         String evenOrOdd = "";
-        int numberCopy = 3242592;
+        int copyNumberCheckTwos = numberCheckTwos;
 
-        while(numberCopy > 0) {
-            if (numberCopy % 10 == 2) {
+        while(copyNumberCheckTwos > 0) {
+            if (copyNumberCheckTwos % 10 == 2) {
                 countTwos++;
             }
-            numberCopy /= 10;
+            copyNumberCheckTwos /= 10;
         }
 
         if (countTwos % 2 == 0) {
@@ -89,7 +89,7 @@ public class CyclesTheme {
         } else {
             evenOrOdd = "(нечетное)";
         }
-        System.out.printf("В %d %s количество двоек - %s%n", number, evenOrOdd, countTwos);
+        System.out.printf("В %d %s количество двоек - %s%n", numberCheckTwos, evenOrOdd, countTwos);
 
         System.out.println("\n6. Отображение геометрических фигур\n");
         for (int i = 0; i < 5; i++) {
@@ -100,58 +100,58 @@ public class CyclesTheme {
         }
         System.out.println();
 
-        int i = 5;
-        while (i > 0) {
-            int j = i;
-            while (j > 0) {
+        int row = 5;
+        while (row > 0) {
+            int column = row;
+            while (column > 0) {
                 System.out.print('#');
-                j--;
+                column--;
             }
             System.out.println();
-            i--;
+            row--;
         }
 
-        a = 3;
-        b = 0;
-        i = 1;
+        int attempt = 3;
+        int columnReduction = 0;
+        row = 1;
         do {
             System.out.print("\n");
-            int j = 1;
+            int column = 1;
             do {
                 System.out.print("$");
-                j++;
-            } while (j <= i - b);
+                column++;
+            } while (column <= row - columnReduction);
             
-            if (i >= a) {
-                b += 2;
+            if (row >= attempt) {
+                columnReduction += 2;
             }
-            i++;
-        } while (i < a * 2);
+            row++;
+        } while (row < attempt * 2);
         System.out.println();
 
         System.out.println("\n7. Отображение ASCII-символов\n");
         System.out.printf("%8s %10s %13s%n", "DECIMAL", "CHARACTER", "DESCRIPTION");
         System.out.println("-------------------------------------------------");
-        for (i = 15; i < 48; i++) {
+        for (int i = 15; i < 48; i++) {
             if (i % 2 == 1) {
                 System.out.printf("%5d %9c %5s %-13s%n", i, (char)i, " ", Character.getName(i));
             }
         }
 
-        for (i = 97; i < 123; i++) {
+        for (int i = 97; i < 123; i++) {
             if (i % 2 == 0) {
                 System.out.printf("%5d %9c %5s %-13s%n", i, (char)i, " ", Character.getName(i));
             }
         }
 
         System.out.println("\n8. Проверка, является ли число палиндромом");
-        number = 1234321;
+        int palindromeInt = 1234321;
         int reversedInt = 0;
         int remainder;
-        int originalInt = number;
+        int originalInt = palindromeInt;
          
-        for( ;number != 0; number /= 10 ) {
-            remainder = number % 10; 
+        for( ;palindromeInt != 0; palindromeInt /= 10 ) {
+            remainder = palindromeInt % 10; 
             reversedInt = reversedInt * 10 + remainder;
         }
         if (originalInt == reversedInt) {
@@ -166,11 +166,11 @@ public class CyclesTheme {
         String st1 = "";
         String st2 = "";
         counter = 0;
-        number = 234801;
-        numberCopy = 234801;
+        int happyInt = 234801;
+        int copyHappyInt = 234801;
 
-        while (number > 0) {
-            int temp = number % 10;
+        while (happyInt > 0) {
+            int temp = happyInt % 10;
             if (counter < 3) {
                 sum1 += temp;
                 st1 += temp;
@@ -178,13 +178,13 @@ public class CyclesTheme {
                 sum2 += temp;
                 st2 += temp;
             }
-            number /= 10;
+            happyInt /= 10;
             counter++;
         }
         if (sum1 == sum2) {
-            System.out.printf("Число %s является счастливым%n", numberCopy);
+            System.out.printf("Число %s является счастливым%n", copyHappyInt);
         } else {
-            System.out.printf("Число %s не является счастливым%n", numberCopy);
+            System.out.printf("Число %s не является счастливым%n", copyHappyInt);
         }
         System.out.printf("Сумма цифр %s = %d, а сумма %s = %d%n", st1, sum1, st2, sum2);
 
@@ -192,11 +192,11 @@ public class CyclesTheme {
         System.out.println("\n10. Отображение таблицы умножения Пифагора\n");
         st1 = " ";
         System.out.printf("%3s|", st1);
-        for (i = 2; i < 10; i++) {
+        for (int i = 2; i < 10; i++) {
             System.out.printf("%4d", i);
         }
         System.out.println("\n---|---------------------------------");
-        for (i = 2; i < 10;  i++) {
+        for (int i = 2; i < 10;  i++) {
             System.out.printf("%2d |", i);
             for (int j = 2; j < 10; j++) {
                 System.out.printf("%4d", i * j);
