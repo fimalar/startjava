5e7948245193105293d7ad75dfd83ded04441e33
fimalar <nebasineva12@gmail.com>
2023-11-05 12:55:57 +0300

    Убрана лишняя проверка, угадывание числа через среднее арифметическое, переименованы переменные

--- src/MyFirstGame.java
+++ src/MyFirstGame.java
@@ -1,23 +1,21 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int numberPc = 90;
-        int numberPlayer = 7;
+        int pcNumber = 90;
+        int playerNumber = 7;
         int startRange = 1;
         int endRange = 100;
 
-        while (numberPlayer != numberPc) {
-            if ( numberPlayer > numberPc) {
-                System.out.printf("Число %d больше числа, что загадал компьютер\n", numberPlayer);
-                endRange = numberPlayer;
-                numberPlayer -= (endRange - startRange) / 2;
-            } else if (numberPlayer < numberPc) {
-                System.out.printf("Число %d меньше того, что загадал компьютер\n", numberPlayer);
-                startRange = numberPlayer;
-                numberPlayer += (endRange - startRange) / 2;
-            }
-            if (numberPlayer == numberPc) {
-                System.out.println("\"Вы победили!\"");
+        while (playerNumber != pcNumber) {
+            if (playerNumber > pcNumber) {
+                System.out.printf("Число %d больше числа, что загадал компьютер\n", playerNumber);
+                endRange = playerNumber;
+                playerNumber = (startRange + endRange) / 2;
+            } else if (playerNumber < pcNumber) {
+                System.out.printf("Число %d меньше того, что загадал компьютер\n", playerNumber);
+                startRange = playerNumber;
+                playerNumber = (startRange + endRange) / 2;
             }
         }
+        System.out.println("\"Вы победили!\"");
     }
 }
