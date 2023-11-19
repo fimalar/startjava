907b5d6d3f27901f487343a35411ee718cbc6ec7
fimalar <nebasineva12@gmail.com>
2023-11-19 19:54:54 +0300

    Переменная s переименована в input
    Использованы геттеры и сеттеры
    При проверке использована сокращенная запись boolean-значений

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,24 +4,24 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        String s = "";
+        String input = "";
         do {    
             System.out.print("Введите первое число: ");
-            calculator.firstNumber = scan.nextInt();
+            calculator.setA(scan.nextInt());
             scan.nextLine();
             System.out.print("Введите знак математической операции: ");
-            calculator.sign = scan.nextLine().charAt(0);
+            calculator.setSign(scan.nextLine().charAt(0));
             System.out.print("Введите второе число: ");
-            calculator.secondNumber = scan.nextInt();
-            calculator.run();
+            calculator.setB(scan.nextInt());
+            calculator.calc();
             scan.nextLine();
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                s = scan.nextLine();
-                if (s.equals("no") == true) {
+                input = scan.nextLine();
+                if (input.equals("no")) {
                     break;
                 }
-            } while (s.equals("yes") == false);
-        } while (s.equals("yes"));
+            } while (!input.equals("yes"));
+        } while (input.equals("yes"));
     }
 }
