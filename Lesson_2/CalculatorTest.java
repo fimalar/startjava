380da8df9410e1f1ce5d2422e447bf174bb6f653
fimalar <nebasineva12@gmail.com>
2023-11-23 13:10:33 +0300

    Объединены проверки 'Хотите продолжить вычисления?'

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -13,15 +13,12 @@ public class CalculatorTest {
             calculator.setSign(scan.nextLine().charAt(0));
             System.out.print("Введите второе число: ");
             calculator.setB(scan.nextInt());
-            calculator.calc();
             scan.nextLine();
+            calculator.calc();
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
                 input = scan.nextLine();
-                if (input.equals("no")) {
-                    break;
-                }
-            } while (!input.equals("yes"));
+            } while (!input.equals("yes") && !input.equals("no"));
         } while (input.equals("yes"));
     }
 }
