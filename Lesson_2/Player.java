1df542c618b62f14b9ea571daa0e6bc11fb0f6f5
fimalar <nebasineva12@gmail.com>
2023-11-29 11:14:52 +0300

    Создан класс Player.jaav

---
+++ Lesson_2/Player.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Player {
+    private String name;
+    private int number;
+
+    Player(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
