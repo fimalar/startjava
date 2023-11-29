b74c6b0e4c099c8b2e5316657d3a38f256ee61f4
fimalar <nebasineva12@gmail.com>
2023-11-29 21:49:49 +0300

    Добавлены геттер и сеттер переменной number

--- Lesson_2/Player.java
+++ Lesson_2/Player.java
@@ -1,14 +1,21 @@
-import java.util.Scanner;
 
 public class Player {
     private String name;
     private int number;
 
-    Player(String name){
+    Player(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
     }
 }
