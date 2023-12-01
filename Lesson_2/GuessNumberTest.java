a5ed16c163d093fa458c36b88ab7736383c008a2
fimalar <nebasineva12@gmail.com>
2023-12-01 21:18:29 +0300

    Удален из класса метод start(), вложенный цикл изменен на do-while
    Переименованы переменные firstPlayer, secondPlayer  -> player1, player2

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -4,19 +4,18 @@ public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите имя первого игрока");
-        Player firstPlayer = new Player(sc.nextLine());
+        Player player1 = new Player(sc.nextLine());
         System.out.println("Введите имя второго игрока");
-        Player secondPlayer = new Player(sc.nextLine());
-        GuessNumber guessNumber = new GuessNumber(firstPlayer, secondPlayer);
-        String inputYesNo = "yes";
-        while (inputYesNo.equals("yes")) {
-            guessNumber.start();
+        Player player2 = new Player(sc.nextLine());
+        GuessNumber guessNumber = new GuessNumber(player1, player2);
+        String inputYesNo = "";
+        do {
             guessNumber.play();
             System.out.println("Хотите продолжить игру? [yes/no]: ");
             inputYesNo = sc.nextLine();
             while (!inputYesNo.equals("yes") && !inputYesNo.equals("no")) {
                 System.out.println("Хотите продолжить игру? [yes/no]: ");
             }
-        }
+        } while (inputYesNo.equals("yes"));
     }
 }
