7eafc1fed3390cf7ea9c8abb2358938de483b7f9
fimalar <nebasineva12@gmail.com>
2023-12-26 15:49:35 +0300

    Каждое задание реализовано в отдельном методе, даны осмысленные имена переменным

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -6,126 +6,80 @@ import java.util.Random;
 public class ArrayTheme {
     public static void main(String[] args) {
         System.out.println("\n1. Реверс значений массива");
-        // заполнение массива числами вразнобой в отрезке [1, 7]
-        int[] numbs1 = {7, 5, 1, 4, 6, 2, 3};
-        int len = numbs1.length;
-        // вывод значений массива до реверса
-        System.out.println("До реверса: " + Arrays.toString(numbs1));
-        // перестановка значения в исходном массиве в обратном порядке
-        reverse(numbs1);
-        // вывод значений массива после реверса
-        System.out.println("После реверса: " + Arrays.toString(numbs1));
+        reverse();
 
         System.out.println("\n2. Произведение элементов массива");
-        int[] numbs2 = new int[10];
-        initializationArray(numbs2);
-        // подсчет произведения
-        int multi = multi(numbs2, 1, 9);
-        // отобразить произведение
-        System.out.println(printArray(numbs2, 1, 9) + " = " + multi);
+        multi();
 
         System.out.println("\n3. Удаление элементов массива");
-        double[] numbs3 = new double[15];
-        // заполнение массива 15 случайными вещественными числами в полуинтервале [0, 1)
-        initializationArray(numbs3);
-        // отображение исходного массива по 8 и 7 чисел в строке, после запятой только три знак
-        System.out.println("Исходный массив:");
-        System.out.println("[" + printArray(numbs3, 0, 8) + "]");
-        System.out.println("[" + printArray(numbs3, 8, 15) + "]");
-        // вычисление индекса средней ячейки массива
-        int indexMiddleCell = indexMiddleCell(numbs3);
-        double numbMiddleCell = numbs3[indexMiddleCell];
-        // перезапись в массиве нулями чисел, которые больше значения числа средней ячейки массива
-        rewrite(numbs3, numbMiddleCell);
-        // отображение измененного массива по 8 и 7 чисел в строке, после запятой только три знака
-        System.out.println("\nИзмененный массив:");
-        System.out.println("[" + printArray(numbs3, 0, indexMiddleCell + 1) + "]");
-        System.out.println("[" + printArray(numbs3, indexMiddleCell + 1, 15) + "]");
-        // количество обнуленных ячеек
-        System.out.println("\nКоличество обнуленных ячеек: " + countZero(numbs3));
+        deleteElements();
 
         System.out.println("\n4. Вывод алфавита лесенкой");
-        char[] arrayCapitalLetters = new char[26];
-        // инициализируйте массив в цикле большими анг. буквами по возрастанию
-        initializationArray(arrayCapitalLetters);
-        // отображение его содержимого лесенкой в обратном порядке
-        printArrayStairs(arrayCapitalLetters);
+        printAlphabetStairs();
 
-        System.out.println("\n5/ Заполнение массива уникальными числами");
-        int[] numbs4 = new int[30];
-        // заполнение массива 30-ю случайными целыми числами в полуинтервале [60, 100)
-        // значения не должны повторяться, если число уже есть в массиве, сгенерите новое
-        // делайте это до тех пор, пока не сгенерится число, которого еще нет в массиве
-        initializationArray(numbs4, 30);
-        // отображение содержимого массива по 10 чисел в строке, отсортированного по возрастанию
-        sortArray(numbs4);
-        printArray(numbs4, 10);
+        System.out.println("\n5. Заполнение массива уникальными числами");
+        uniqNumbsInt();
     }
 
-    private static int[] reverse(int[] array) {
-        for (int i = 0; i < array.length / 2; i++) {
-            int temp = array[i];
-            array[i] = array[array.length - 1 - i];
-            array[array.length - 1 - i] = temp;
+    private static void reverse() {
+        int[] numbsInt = {7, 5, 1, 4, 6, 2, 3};
+        int length = numbsInt.length;
+        System.out.println("До реверса: " + Arrays.toString(numbsInt));
+        for (int i = 0; i < length / 2; i++) {
+            int temp = numbsInt[i];
+            numbsInt[i] = numbsInt[length - 1 - i];
+            numbsInt[length - 1 - i] = temp;
         }
-        return array;
+        System.out.println("После реверса: " + Arrays.toString(numbsInt));
     }
 
-    private static int[] initializationArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i;
+    private static void multi() {
+        int[] numbsInt = new int[10];
+        for (int i = 0; i < numbsInt.length; i++) {
+            numbsInt[i] = i;
         }
-        return array;
-    }
-
-    private static int multi(int[] array, int indexFrom, int indexTo) {
         int multi = 1;
-        for (int i = indexFrom; i < indexTo; i++) {
+        for (int i = 1; i < 9; i++) {
             multi *= i;
         }
-        return multi;
-    }
-
-    private static String printArray(int[] array, int indexFrom, int intdexTo) {
         String s = "";
-        for (int i = indexFrom; i < intdexTo ; i++) {
-            s += array[i];
-            if (i == intdexTo -1) {
+        for (int i = 1; i < 9 ; i++) {
+            s += numbsInt[i];
+            if (i == 8) {
                 break;
             }
             s += " * ";
         }
-        return s;
+        System.out.println(s + " = " + multi);
     }
 
-    private static double[] initializationArray(double[] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Math.random();
+    private static void deleteElements() {
+        double[] numbsDouble = new double[15];
+        for (int i = 0; i < numbsDouble.length; i++) {
+            numbsDouble[i] = Math.random();
         }
-        return array;
-    }
-
-    private static int indexMiddleCell(double[] array) {
-        int indexMiddleCell = 0;
-        if (array.length % 2 != 0) {
-            indexMiddleCell = array.length / 2;
-        }
-        return indexMiddleCell;
-    }
-
-    private static void rewrite(double[] array, double numbMiddleCell) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > numbMiddleCell) {
-                array[i] = 0;
+        System.out.println("Исходный массив:");
+        System.out.println("[" + printArray(numbsDouble, 0, 8) + "]");
+        System.out.println("[" + printArray(numbsDouble, 8, 15) + "]");
+        double numbMiddleCell = numbsDouble[numbsDouble.length / 2];
+        int countZero = 0;
+        for (int i = 0; i < numbsDouble.length; i++) {
+            if (numbsDouble[i] > numbMiddleCell) {
+                numbsDouble[i] = 0;
+                countZero++;
             }
         }
+        System.out.println("\nИзмененный массив:");
+        System.out.println("[" + printArray(numbsDouble, 0, 8) + "]");
+        System.out.println("[" + printArray(numbsDouble, 8, 15) + "]");
+        System.out.println("\nКоличество обнуленных ячеек: " + countZero);
     }
 
     private static String printArray(double[] array, int indexFrom, int intdexTo) {
         String s = "";
         for (int i = indexFrom; i < intdexTo ; i++) {
             s += String.format("%6.3f", array[i]);
-            if (i == intdexTo -1) {
+            if (i == intdexTo - 1) {
                 break;
             }
             s += ", ";
@@ -133,67 +87,48 @@ public class ArrayTheme {
         return s;
     }
 
-    private static int countZero(double[] array) {
-        int count = 0;
-        for (double d : array ) {
-            if (d == 0) {
-                count++;
-            }
+    private static void printAlphabetStairs() {
+        char[] CapitalLetters = new char[26];
+        for (int i = 0; i < CapitalLetters.length; i++) {
+            CapitalLetters[i] = (char) (i + 'A');
         }
-        return count;
-    }
-
-    private static char[] initializationArray(char[] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (char) (i + 'A');
-        }
-        return array;
-    }
-
-    private static void printArrayStairs(char[] array) {
-        for (int i = array.length -1; i >= 0; i--) {
-            for (int j = array.length -1; j >= i; j--) {
-                System.out.print(array[j]);
+        for (int i = CapitalLetters.length -1; i >= 0; i--) {
+            for (int j = CapitalLetters.length -1; j >= i; j--) {
+                System.out.print(CapitalLetters[j]);
             }
             System.out.println();
         }
     }
 
-    private static int[] initializationArray(int[] array, int length) {
+    private static void uniqNumbsInt() {
+        int[] uniqNumbsInt = new int[30];
         Random random = new Random();
         int numb;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < uniqNumbsInt.length; i++) {
             numb = random.nextInt(30, 60);
             for (int j = 0; j <= i; j++) {
-                if (numb == array[j]) {
+                if (numb == uniqNumbsInt[j]) {
                     numb = random.nextInt(30, 60);
                     j = 0;
                 }
             }
-            array[i] = numb;
+            uniqNumbsInt[i] = numb;
         }
-        return array;
-    }
-
-    private static void sortArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j+1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+        for (int i = 0; i < uniqNumbsInt.length; i++) {
+            for (int j = 0; j < uniqNumbsInt.length - 1 - i; j++) {
+                if (uniqNumbsInt[j] > uniqNumbsInt[j+1]) {
+                    int temp = uniqNumbsInt[j];
+                    uniqNumbsInt[j] = uniqNumbsInt[j + 1];
+                    uniqNumbsInt[j + 1] = temp;
                 }
             }
         }
-    }
-
-    private static void printArray(int[] array, int countNumbRow) {
-        for (int i = 0; i < array.length ; i++) {
-            if (i % countNumbRow == 0) {
+        for (int i = 0; i < uniqNumbsInt.length ; i++) {
+            if (i % 10 == 0) {
                 System.out.println();
             }
-            System.out.print(array[i]);
-            if (i == array.length -1) {
+            System.out.print(uniqNumbsInt[i]);
+            if (i == uniqNumbsInt.length -1) {
                 break;
             }
             System.out.print(", ");
