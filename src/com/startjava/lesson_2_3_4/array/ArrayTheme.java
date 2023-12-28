e33e00bcae4bc3c102fa2c5ab7b184e8829f41f3
fimalar <nebasineva12@gmail.com>
2023-12-28 12:19:45 +0300

    Переименованы методы и переменные. Каждое задание реализовано в одном методе
    Изменено условие в цикле метода reverseElements() и применен декремент длины массива
    В методе multiplyElements() применен тернарный оператор для вывода, объединено
    умножение и вывод результата в один цикл
    В методе deleteElements() вывод осуществляется вызовом одного метода, удалены [] при выводе массива
    Изменена реализация метода printArray

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -6,85 +6,75 @@ import java.util.Random;
 public class ArrayTheme {
     public static void main(String[] args) {
         System.out.println("\n1. Реверс значений массива");
-        reverse();
+        reverseElements();
 
         System.out.println("\n2. Произведение элементов массива");
-        multi();
+        multiplyElements();
 
-        System.out.println("\n3. Удаление элементов массива");
+        System.out.println("\n\n3. Удаление элементов массива");
         deleteElements();
 
         System.out.println("\n4. Вывод алфавита лесенкой");
         printAlphabetStairs();
 
         System.out.println("\n5. Заполнение массива уникальными числами");
-        uniqNumbsInt();
+        fillUniqueRandomNumbs();
     }
 
-    private static void reverse() {
-        int[] numbsInt = {7, 5, 1, 4, 6, 2, 3};
-        int length = numbsInt.length;
-        System.out.println("До реверса: " + Arrays.toString(numbsInt));
-        for (int i = 0; i < length / 2; i++) {
-            int temp = numbsInt[i];
-            numbsInt[i] = numbsInt[length - 1 - i];
-            numbsInt[length - 1 - i] = temp;
+    private static void reverseElements() {
+        int[] segmentNumbsToSeven = {7, 5, 1, 4, 6, 2, 3};
+        int length = segmentNumbsToSeven.length;
+        System.out.println("До реверса: " + Arrays.toString(segmentNumbsToSeven));
+        for (int i = 0; i < length; i++) {
+            length--;
+            int temp = segmentNumbsToSeven[i];
+            segmentNumbsToSeven[i] = segmentNumbsToSeven[length];
+            segmentNumbsToSeven[length] = temp;
         }
-        System.out.println("После реверса: " + Arrays.toString(numbsInt));
+        System.out.println("После реверса: " + Arrays.toString(segmentNumbsToSeven));
     }
 
-    private static void multi() {
-        int[] numbsInt = new int[10];
-        for (int i = 0; i < numbsInt.length; i++) {
-            numbsInt[i] = i;
+    private static void multiplyElements() {
+        int[] multipliers = new int[10];
+        for (int i = 0; i < multipliers.length; i++) {
+            multipliers[i] = i;
         }
         int multi = 1;
-        for (int i = 1; i < 9; i++) {
+        for (int i = 1; i < multipliers.length - 1; i++) {
             multi *= i;
+            System.out.print(multipliers[i]);
+            System.out.print((i != multipliers.length - 2) ?  " * " : " = " + multi);
         }
-        String s = "";
-        for (int i = 1; i < 9 ; i++) {
-            s += numbsInt[i];
-            if (i == 8) {
-                break;
-            }
-            s += " * ";
-        }
-        System.out.println(s + " = " + multi);
     }
 
     private static void deleteElements() {
-        double[] numbsDouble = new double[15];
-        for (int i = 0; i < numbsDouble.length; i++) {
-            numbsDouble[i] = Math.random();
+        double[] randomDoubles = new double[15];
+        for (int i = 0; i < randomDoubles.length; i++) {
+            randomDoubles[i] = Math.random();
         }
         System.out.println("Исходный массив:");
-        System.out.println("[" + printArray(numbsDouble, 0, 8) + "]");
-        System.out.println("[" + printArray(numbsDouble, 8, 15) + "]");
-        double numbMiddleCell = numbsDouble[numbsDouble.length / 2];
-        int countZero = 0;
-        for (int i = 0; i < numbsDouble.length; i++) {
-            if (numbsDouble[i] > numbMiddleCell) {
-                numbsDouble[i] = 0;
-                countZero++;
+        printArray(randomDoubles);
+        double numbMiddleCell = randomDoubles[randomDoubles.length / 2];
+        int countZeros = 0;
+        for (int i = 0; i < randomDoubles.length; i++) {
+            if (randomDoubles[i] > numbMiddleCell) {
+                randomDoubles[i] = 0;
+                countZeros++;
             }
         }
-        System.out.println("\nИзмененный массив:");
-        System.out.println("[" + printArray(numbsDouble, 0, 8) + "]");
-        System.out.println("[" + printArray(numbsDouble, 8, 15) + "]");
-        System.out.println("\nКоличество обнуленных ячеек: " + countZero);
+        System.out.println("\n\nИзмененный массив:");
+        printArray(randomDoubles);
+        System.out.println("\n\nКоличество обнуленных ячеек: " + countZeros);
     }
 
-    private static String printArray(double[] array, int indexFrom, int intdexTo) {
-        String s = "";
-        for (int i = indexFrom; i < intdexTo ; i++) {
-            s += String.format("%6.3f", array[i]);
-            if (i == intdexTo - 1) {
-                break;
+    private static void printArray(double[] array) {
+        for (int i = 0; i < array.length ; i++) {
+            System.out.printf("%6.3f", array[i]);
+            System.out.print((i != array.length - 1) ? ", " : "");
+            if (i == array.length / 2) {
+                System.out.println();
             }
-            s += ", ";
         }
-        return s;
     }
 
     private static void printAlphabetStairs() {
@@ -92,46 +82,43 @@ public class ArrayTheme {
         for (int i = 0; i < CapitalLetters.length; i++) {
             CapitalLetters[i] = (char) (i + 'A');
         }
-        for (int i = CapitalLetters.length -1; i >= 0; i--) {
-            for (int j = CapitalLetters.length -1; j >= i; j--) {
+        for (int i = CapitalLetters.length - 1; i >= 0; i--) {
+            for (int j = CapitalLetters.length - 1; j >= i; j--) {
                 System.out.print(CapitalLetters[j]);
             }
             System.out.println();
         }
     }
 
-    private static void uniqNumbsInt() {
-        int[] uniqNumbsInt = new int[30];
+    private static void fillUniqueRandomNumbs() {
+        int[] uniqueRandomNumbs = new int[30];
         Random random = new Random();
         int numb;
-        for (int i = 0; i < uniqNumbsInt.length; i++) {
+        for (int i = 0; i < uniqueRandomNumbs.length; i++) {
             numb = random.nextInt(30, 60);
             for (int j = 0; j <= i; j++) {
-                if (numb == uniqNumbsInt[j]) {
+                if (numb == uniqueRandomNumbs[j]) {
                     numb = random.nextInt(30, 60);
                     j = 0;
                 }
             }
-            uniqNumbsInt[i] = numb;
+            uniqueRandomNumbs[i] = numb;
         }
-        for (int i = 0; i < uniqNumbsInt.length; i++) {
-            for (int j = 0; j < uniqNumbsInt.length - 1 - i; j++) {
-                if (uniqNumbsInt[j] > uniqNumbsInt[j+1]) {
-                    int temp = uniqNumbsInt[j];
-                    uniqNumbsInt[j] = uniqNumbsInt[j + 1];
-                    uniqNumbsInt[j + 1] = temp;
+        for (int i = 0; i < uniqueRandomNumbs.length; i++) {
+            for (int j = 0; j < uniqueRandomNumbs.length - 1 - i; j++) {
+                if (uniqueRandomNumbs[j] > uniqueRandomNumbs[j + 1]) {
+                    int temp = uniqueRandomNumbs[j];
+                    uniqueRandomNumbs[j] = uniqueRandomNumbs[j + 1];
+                    uniqueRandomNumbs[j + 1] = temp;
                 }
             }
         }
-        for (int i = 0; i < uniqNumbsInt.length ; i++) {
+        for (int i = 0; i < uniqueRandomNumbs.length ; i++) {
             if (i % 10 == 0) {
                 System.out.println();
             }
-            System.out.print(uniqNumbsInt[i]);
-            if (i == uniqNumbsInt.length -1) {
-                break;
-            }
-            System.out.print(", ");
+            System.out.print(uniqueRandomNumbs[i]);
+            System.out.print((i != uniqueRandomNumbs.length - 1) ? ", " : "");
         }
     }
 }
